b654d5ff4359f8a5c8ce2ed786edb857d1e188b9
MasaruTakahashi <MasaruTakahashi@users.noreply.github.com>
2019-03-22 18:52:05 +0900

    q1

---
+++ Q1.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE q1;
+USE q1;
+CREATE TABLE item_category(category_id int,category_name VARCHAR(256));
+
---
+++ Q2.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE q2;
+USE q2;
+CREATE TABLE item(item_id int,item_name VARCHAR(256),Item_price int,category_id int);
+SELECT * FROM item;
